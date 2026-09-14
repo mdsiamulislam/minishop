30eaa9195035053fe72ead2640df4392a2b19162
Siam <mdsiamulislams@gmail.com>
2026-09-14 11:06:11 +0600

    Add admin_shell app and update URL routing

--- minishop/settings.py
+++ minishop/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'admin_shell',
+
     # User-defined apps
     'apps.authentication',
     'apps.common',
